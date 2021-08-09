13fa513c22c7f5bbb765147fd299362e237b8e59
theandrewfulton <zenithsprings1@yahoo.com.au>
2021-08-09 22:18:12 +1000

    return shared lists from backend

--- src/scenes/list-search/index.jsx
+++ src/scenes/list-search/index.jsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
 
+// grab backend to use based on environment
+import { backend } from "../../data"
 
 export const ListSearch = () => {
+    const [lists, setLists] = useState([])
+    // const [title, setTitle] = useState("")
+    // const [description, setDescription] = useState("")
+    // const [shared, setShared] = useState(false)
+    const [loading, setLoading] = useState(true)
+    const [errorMessage, setErrorMessage] = useState("")
+        // get lists from backend
+        useEffect(() => {
+            backend.get("/search/list_search")
+                .then(({ data }) => setLists(data))
+                .catch((error) => setErrorMessage(error.message))
+                .finally(() => setLoading(false))
+        }, [])
+
     return(
-        <p>List Search</p>
+
+        // <p>List Search</p>
+<>
+{errorMessage}
+{loading && <p>Loading...</p>}
+        {lists.map(({id, title, description, shared }, index) => (
+            // public is a reserved word so need to revisit and rename
+            // to shared then add to destructuring assignment above
+            // add username to lists
+            <article key={id}>
+              <Link key={id} to={`/lists/${id}`}>
+              <p>{title}</p>
+              <p>{description}</p>
+              {shared && <p>shared</p>}
+              </Link>
+            </article>
+            
+          ))}
+          </>
     )
 }
