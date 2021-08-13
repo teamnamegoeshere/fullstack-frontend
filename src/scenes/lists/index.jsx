8cc33eae81afa2912ee2b535c2a92635ff0c2300
Timothy Dunk <tim@dunk.id.au>
2021-08-13 10:43:39 +1000

    removed second create list function

--- src/scenes/lists/index.jsx
+++ src/scenes/lists/index.jsx
@@ -8,8 +8,6 @@ import { backend } from "../../data"
 export const Lists = () => {
 
     const [lists, setLists] = useState([])
-    const [title, setTitle] = useState("")
-    const [description, setDescription] = useState("")
     // const [shared, setShared] = useState(false)
     const [loading, setLoading] = useState(true)
     const [errorMessage, setErrorMessage] = useState("")
@@ -23,44 +21,7 @@ export const Lists = () => {
             .finally(() => setLoading(false))
     }, [])
 
-    // Create list
-    const createList = async (e) => {
-    // prevent default form behaviour
-    e.preventDefault()
-    // set loading state to true
-    setLoading(true)
-    // unset error message
-    setErrorMessage("")
-    // send post request to the backend
-    try {
-        const { data } = await backend.post("/lists", {
-          title,
-          description
-        // shared,
-        })
-        // clone list of lists and add new entry to the list
-        const listsClone = [...lists]
-        // add new list to cloned array
-          listsClone.push({
-            title: data.title,
-            description: data.description,
-            // shared,
-            id: data.id
-          })
-          // set cloned array of lists as state
-          setLists(listsClone)
-        // if success:
-    } catch (error) {
-        // If Fail:
-        // display error message to the user
-        setErrorMessage(error.message)
-        // stop loading
-        setLoading(false)
-    } finally {
-      setLoading(false)
-    }
     
-  }
   // List delete method
   // Do we want to delete from lists screen or click on individual list first?
     const deleteList = async (id, index) => {
@@ -113,17 +74,7 @@ export const Lists = () => {
         {/* create list button */}
         <Link to ="/lists/create"><button>New List</button></Link>
        
-       {/* Create list form */}
-          <form onSubmit={createList}>
-            <input onChange={(e) => setTitle(e.target.value)} value={title} placeholder="title" />
-            <input onChange={(e) => setDescription(e.target.value)} value={description} placeholder="description" />
-            {/* <label>
-              Share this list
-            <input onChange={(e) => setShared(e.target.value)} value={shared} type="checkbox"/>
-            </label> */}
-            <input type="submit" value="Submit" />
-            
-          </form>
+       
         </div>
     )
 
